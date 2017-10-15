daf69c42c57aab1957956eb9bfb05972732fd5b4
Melissa Bower <melissaclausen@boisestate.edu>
2017-10-14 19:17:33 -0600

    heyo

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.appother')
 
 @section('content')
-<div class="container">
+<div class="container pull-right">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
